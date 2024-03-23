a98d1d01d44efcf7a78aafe52db72766d9b76ec2
daniyall_sidikii <160391502+daniallsidikii@users.noreply.github.com>
2024-03-23 17:17:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "firstNum",
+        message: "enter first number: ",
+    },
+    {
+        type: "number",
+        name: "secondNum",
+        message: "enter second number: ",
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "choose your operator: ",
+        choices: ["+", "-", "x", "/"]
+    }
+]);
+let number1 = answer.firstNum;
+let number2 = answer.secondNum;
+let optr = answer.operator;
+if (optr === "+") {
+    console.log("the sum is: ", number1 + number2);
+}
+else if (optr === "-") {
+    console.log("the substraction is: ", number1 - number2);
+}
+else if (optr === "*") {
+    console.log("the multiplication is: ", number1 * number2);
+}
+else {
+    console.log("the division is is: ", number1 / number2);
+}
